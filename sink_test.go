a589797bc8662877ee120f13dab91954306b75fa
Daniel Wentland <daniel.wentland@gmail.com>
2020-10-27 12:42:20 +0100

    Add test case for files with whitespace

--- sink_test.go
+++ sink_test.go
@@ -12,6 +12,49 @@ import (
 	scp "github.com/hnakamur/go-scp"
 )
 
+func TestWhitespaceInFileName(t *testing.T) {
+	s, l, err := newTestSshdServer()
+	if err != nil {
+		t.Fatalf("fail to create test sshd server; %s", err)
+	}
+	defer s.Close()
+	go s.Serve(l)
+
+	c, err := newTestSshClient(l.Addr().String())
+	if err != nil {
+		t.Fatalf("fail to serve test sshd server; %s", err)
+	}
+	defer c.Close()
+
+	localDir, err := ioutil.TempDir("", "go-scp-TestReceiveDir-local")
+	if err != nil {
+		t.Fatalf("fail to get tempdir; %s", err)
+	}
+	defer os.RemoveAll(localDir)
+
+	remoteDir, err := ioutil.TempDir("", "go-scp-TestReceiveDir-remote")
+	if err != nil {
+		t.Fatalf("fail to get tempdir; %s", err)
+	}
+	defer os.RemoveAll(remoteDir)
+
+	entries := []fileInfo{
+		{name: "foo", maxSize: testMaxFileSize, mode: 0644},
+		{name: "white test", maxSize: testMaxFileSize, mode: 0600},
+	}
+	err = generateRandomFiles(remoteDir, entries)
+	if err != nil {
+		t.Fatalf("fail to generate remote files; %s", err)
+	}
+
+	err = scp.NewSCP(c).ReceiveDir(remoteDir, localDir, nil)
+	if err != nil {
+		t.Errorf("fail to ReceiveDir; %s", err)
+	}
+	remoteDirBase := filepath.Base(remoteDir)
+	localDestDir := filepath.Join(localDir, remoteDirBase)
+	sameDirTreeContent(t, remoteDir, localDestDir)
+}
 func TestReceiveFile(t *testing.T) {
 	s, l, err := newTestSshdServer()
 	if err != nil {
